8acb1f6890e51e55d9dc3f6fed7a9a27fc92a8ff
rich-alien <Richalien@yandex.ru>
2020-07-07 15:03:29 +0400

    update style the SettingBar

---
+++ src/components/Main/TotalItem/Item/Item.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "./Item.module.css"
+
+const Item = (props) => {
+    return (
+        <div className={style.container}>
+            <div className={style.ObjName}>
+                {
+                    props.ObjName
+                }
+            </div>
+            <div className={style.ObjImg}>
+                {
+                    props.ObjImg
+                }
+            </div>
+            <div className={style.ObjPrice}>
+                {
+                    props.ObjPrice
+                }
+            </div>
+            <button className={style.btn}>
+                OPEN
+            </button>
+        </div>
+    )
+}
+export default Item
--- src/components/Main/TotalItem/TotalItem.jsx
+++ src/components/Main/TotalItem/TotalItem.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import style from './TotalItem.module.css'
+import style from './TotalItem.module.css';
+import Item from "./Item/Item";
 // заменить sortUp and SortDown на иконки
+let ObjItem = [
+    {ObjName:'Fuck1',ObjImg:'fuck2',ObjPrice:'123rub'},
+    {ObjName:'Fuck1',ObjImg:'fuck2',ObjPrice:'123rub'},
+    {ObjName:'Fuck1',ObjImg:'fuck2',ObjPrice:'123rub'},
+    {ObjName:'Fuck1',ObjImg:'fuck2',ObjPrice:'123rub'},
+    {ObjName:'Fuck1',ObjImg:'fuck2',ObjPrice:'123rub'},
+]
+
 const TotalItem = () => {
+let    ElementItem = ObjItem.map(e=> <Item ObjName={e.ObjName} ObjImg={e.ObjImg} ObjPrice={e.ObjPrice}/>)
     return (
         <div className={style.container}>
         <div className={style.SettingBar}>
-            <button className={style.Standart}>
-                а....я
-            </button>
+
             <div className={style.sort}>
-                <button className={style.sortUp}>
-                    по цене, дорогие
+                <button className={`${style.btn} ${style.StanDart}`}>
+                    {/*изменить className*/}
+                    а....я
                 </button>
-                <button className={style.sortDown}>
-                    по цене, дешевые
-                </button>
+               <div>
+                   <button className={`${style.btn} ${style.sortUp}`}>
+                       по цене, дорогие
+                   </button>
+                   <button className={`${style.btn} ${style.sortDown}`}>
+                       по цене, дешевые
+                   </button>
+               </div>
             </div>
         </div>
-
+            {
+                ElementItem
+            }
         </div>
     )
 }
