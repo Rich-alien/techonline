dca6cf3932401305e14c3bc480e39447faf6b726
rich-alien <Richalien@yandex.ru>
2020-07-11 13:41:02 +0400

    add Cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Cart.module.css"
+import NavNar from "../Main/NavBar/NavBar";
+
+const Cart = () =>{
+    return(
+        <div className={style.container}>
+            <NavNar/>
+        </div>
+
+    )
+}
+export default Cart
