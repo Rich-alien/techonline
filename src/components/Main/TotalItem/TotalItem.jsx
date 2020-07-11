7c60735f41fedfa1f8acd338df2ca86787e9540f
rich-alien <Richalien@yandex.ru>
2020-07-11 19:02:35 +0400

    delete ObjItem from the TotalItem and move to state

--- src/components/Main/TotalItem/TotalItem.jsx
+++ src/components/Main/TotalItem/TotalItem.jsx
@@ -2,48 +2,34 @@ import React from 'react';
 import style from './TotalItem.module.css';
 import Item from "./Item/Item";
 // заменить sortUp and SortDown на иконки
-let ObjItem = [
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-    {ObjName:'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',ObjImg:'SomeImg',ObjPrice:'123rub'},
-]
 
-const TotalItem = () => {
-let    ElementItem = ObjItem.map(e=> <Item ObjName={e.ObjName} ObjImg={e.ObjImg} ObjPrice={e.ObjPrice}/>)
+
+const TotalItem = (props) => {
+    let ElementItem = props.ObjItem.map(e => <Item ObjName={e.ObjName} ObjImg={e.ObjImg} ObjPrice={e.ObjPrice}/>)
     return (
         <div className={style.container}>
-        <div className={style.SettingBar}>
+            <div className={style.SettingBar}>
 
-            <div className={style.sort}>
-                <button className={`${style.btn} ${style.StanDart}`}>
-                    {/*изменить className*/}
-                    а....я
-                </button>
-               <div>
-                   <button className={`${style.btn} ${style.sortUp}`}>
-                       по цене, дорогие
-                   </button>
-                   <button className={`${style.btn} ${style.sortDown}`}>
-                       по цене, дешевые
-                   </button>
-               </div>
+                <div className={style.sort}>
+                    <button className={`${style.btn} ${style.StanDart}`}>
+                        {/*изменить className*/}
+                        а....я
+                    </button>
+                    <div>
+                        <button className={`${style.btn} ${style.sortUp}`}>
+                            по цене, дорогие
+                        </button>
+                        <button className={`${style.btn} ${style.sortDown}`}>
+                            по цене, дешевые
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-          <div className={style.catalog}>
-              {
-                  ElementItem
-              }
-          </div>
+            <div className={style.catalog}>
+                {
+                    ElementItem
+                }
+            </div>
         </div>
     )
 }
