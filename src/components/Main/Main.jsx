8a5216599882663b8229313cf22c678562d1800b
rich-alien <Richalien@yandex.ru>
2020-07-11 18:50:03 +0400

    fix

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import style from './Main.module.css'
-// import ChooseCar from "./ChooseCar/ChooseCar";
-// import ChooseCategory from "./ChooseCategory/ChooseCategoty";
+import ChooseCar from "./ChooseCar/ChooseCar";
+import ChooseCategory from "./ChooseCategory/ChooseCategoty";
+import SubChooseCategory from "./SubChooseCategoty/SubChooseCategory";
 import TotalItem from "./TotalItem/TotalItem";
-// import SubChooseCategory from "./SubChooseCategoty/SubChooseCategory";
+import NavNar from "./NavBar/NavBar";
 
 
 const Main = (props) => {
     return (
         <div className={style.main}>
-        {/*<ChooseCar data={props.data}/>*/}
-        {/*<ChooseCategory category={props.category} />*/}
-        {/*<SubChooseCategory SubCategory={props.SubCategory} />*/}
+            <NavNar/>
+            <ChooseCar data={props.data}/>
+            <ChooseCategory category={props.category}/>
+            <SubChooseCategory SubCategory={props.SubCategory}/>
 
-        <TotalItem/>
+            <TotalItem ObjItem={props.ObjItem}/>
         </div>
     )
 }
