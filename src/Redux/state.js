98e2289d3740e52356cce43336872bf65495593a
rich-alien <Richalien@yandex.ru>
2020-07-14 11:18:53 +0400

    add store  and pattern

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,54 +1,118 @@
+ let store = {
+    _state: {
+        mainPage: {
+            ObjItem: [
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+                {
+                    ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff',
+                    ObjImg: 'SomeImg',
+                    ObjPrice: '123rub'
+                },
+            ],
+            data: [
+                {id: '0', CarName: 'Lada Granta', CarImg: 'IMG'},
+                {id: '1', CarName: 'Lada Granta', CarImg: 'IMG'},
+                {id: '2', CarName: 'Lada Granta', CarImg: 'IMG'},
+                {id: '3', CarName: 'Lada Granta', CarImg: 'IMG'},
+                {id: '4', CarName: 'Другое', CarImg: 'обводка машины'},
 
-let state = {
-     ObjItem : [
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-        {ObjName: 'some rfsdvdsfbefvbsdvsdvdsfvsd dsvdfffffffffffffffffffff', ObjImg: 'SomeImg', ObjPrice: '123rub'},
-    ],
-     data : [
-        {id:'0',CarName: 'Lada Granta', CarImg: 'IMG'},
-        {id:'1',CarName: 'Lada Granta', CarImg: 'IMG'},
-        {id:'2',CarName: 'Lada Granta', CarImg: 'IMG'},
-        {id:'3',CarName: 'Lada Granta', CarImg: 'IMG'},
-        {id:'4',CarName: 'Другое', CarImg: 'обводка машины'},
+            ],
+            SubCategory: [
+                {SubCategoryName: 'SubCategoryName'},
+                {SubCategoryName: 'SubCategoryName'},
+                {SubCategoryName: 'SubCategoryName'},
+            ],
+            category: [
+                {Category: 'Lin1'},
+                {Category: 'Lin2'},
+                {Category: 'Lin3'},
+                {Category: 'Lin4'},
+                {Category: 'Lin5'},
+                {Category: 'Lin6'},
+                {Category: 'Lin7'},
+                {Category: 'Lin8'},
+                {Category: 'Lin9'},
+                {Category: 'Lin10'},
+                {Category: 'Lin11'},
+                {Category: 'Lin12'},
+                {Category: 'Lin13'},
+                {Category: 'Lin14'},
+                {Category: 'Lin15'},
+                {Category: 'Lin16'},
 
-    ],
-     SubCategory:[
-        {SubCategoryName:'SubCategoryName'},
-        {SubCategoryName:'SubCategoryName'},
-        {SubCategoryName:'SubCategoryName'},
-    ],
-     category :[
-        {Category:'Lin1'},
-        {Category:'Lin2'},
-        {Category:'Lin3'},
-        {Category:'Lin4'},
-        {Category:'Lin5'},
-        {Category:'Lin6'},
-        {Category:'Lin7'},
-        {Category:'Lin8'},
-        {Category:'Lin9'},
-        {Category:'Lin10'},
-        {Category:'Lin11'},
-        {Category:'Lin12'},
-        {Category:'Lin13'},
-        {Category:'Lin14'},
-        {Category:'Lin15'},
-        {Category:'Lin16'},
+            ],
 
-    ],
+
+        }
+    },
+    _callSubscriber() {
+       console.log('state changed')
+    },
+    getState() {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
-//сделать подобъекты на каждую страничку
-// то есть mainPage
-// searchPage и т.д.
-export default state;
+
+export default store;
