c22373f878a76cd223a81f8db091fd2dca631062
rich-alien <Richalien@yandex.ru>
2020-07-16 13:38:52 +0400

    add closeButton for category , add style and svg icon

---
+++ src/components/CloseButton/closeButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./CloseButton.module.css"
+const closeCar = () =>{
+    return(
+        <div className={style.container}>
+
+        </div>
+    )
+}
+export default closeCar;
