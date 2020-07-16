74a6e1194130827b49e4dadf3b180970d24d1d72
rich-alien <Richalien@yandex.ru>
2020-07-16 20:23:35 +0400

    import img for close btn

--- src/components/CloseButton/ButtonClose.jsx
+++ src/components/CloseButton/ButtonClose.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "./CloseButton.module.css"
-const closeCar = () =>{
+import closeBtnSvg from "../.././close.svg"
+const buttonClose = () =>{
     return(
         <div className={style.container}>
-
+            <img src={closeBtnSvg} alt="logo"/>
         </div>
     )
 }
-export default closeCar;
+export default buttonClose;
