e0bb550d5b7d1d560dec5fe22e98a9aae4b036c2
rich-alien <Richalien@yandex.ru>
2020-07-16 20:27:38 +0400

    add function for close and open

--- src/components/Main/ChooseCategory/ChooseCategoty.jsx
+++ src/components/Main/ChooseCategory/ChooseCategoty.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import style from './ChooseCategory.module.css'
 import CategoryItem from "./CategoryItem/CategoryItem";
-
+import {closeCategory} from "../../../Redux/state";
+// import ButtonClose from "../../CloseButton/ButtonClose";
+import closeBtnSvg from "../../.././close.svg"
 
 const ChooseCategory = (props) => {
     let item = props.category.map(e=><CategoryItem
+        dispatch={props.dispatch}
+        key={e.id}
         Category={e.Category}
-        // stateDisplay = {props.stateDisplay}
-
     />);
-    //props.stateDisplay.categoryDisplay
-    // let styleDisplay = 'none';
     let styleDisplay = props.categoryDisplay;
-    let inform =()=>{
-
+    let goBack=()=>{
+        props.dispatch(closeCategory())
     }
     return (
-        <div className={style.container}>
+        <div style={{display: styleDisplay}} className={style.container}>
+            <div className={style.underBlock}>
 
-            <div style={{display: styleDisplay}} >
-                {
+                <div className={style.text}>
+                    {
+                        props.titleUnderBlock.titleChooseCategory
+                    }
+                </div>
 
-                    item
-                }
+                    <img onClick={goBack} className={style.btn_svg} src={closeBtnSvg} alt="btn-back"/>
 
             </div>
-            <button onClick={inform}></button>
+            <div  className={style.categoryContainer}>
+                {
+                    item
+                }
+            </div>
+
         </div>
     )
 }
