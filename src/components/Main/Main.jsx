b73e6d7a58e44e309f5bf9c615dae3e31e34eb83
rich-alien <Richalien@yandex.ru>
2020-07-16 20:28:54 +0400

    props props and props

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,30 +5,36 @@ import ChooseCategory from "./ChooseCategory/ChooseCategoty";
 import SubChooseCategory from "./SubChooseCategoty/SubChooseCategory";
 import TotalItem from "./TotalItem/TotalItem";
 import NavNar from "./NavBar/NavBar";
-// import Reviews from "../ItemDescription/Reviews/Reviews";
 
 
 const Main = (props) => {
     return (
         <div className={style.main}>
             <NavNar/>
-            {/*<Reviews*/}
-            {/*    Reviews={props.DescriptionPage.Reviews}*/}
-            {/*    dispatch={props.dispatch}*/}
-            {/*/>*/}
             <ChooseCar
+                titleUnderBlock={props.mainPage.titleUnderBLock}
                 data={props.mainPage.data}
                 dispatch={props.dispatch}
+                chooseCar={props.styleBlock.chooseCar}
+
             />
             <ChooseCategory
+                titleUnderBlock={props.mainPage.titleUnderBLock}
                 category={props.mainPage.category}
-                categoryDisplay={props.categoryDisplay}
+                dispatch={props.dispatch}
+                categoryDisplay={props.styleBlock.categoryDisplay}
             />
             <SubChooseCategory
+                titleUnderBlock={props.mainPage.titleUnderBLock}
                 SubCategory={props.mainPage.SubCategory}
+                dispatch={props.dispatch}
+                subCategoryDisplay={props.styleBlock.subCategoryDisplay}
             />
             <TotalItem
+                titleUnderBlock={props.mainPage.titleUnderBLock}
                 ObjItem={props.mainPage.ObjItem}
+                totalItem={props.styleBlock.totalItem}
+                dispatch={props.dispatch}
             />
         </div>
     )
