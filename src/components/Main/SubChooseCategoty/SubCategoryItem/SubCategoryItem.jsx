fd1fee2dbe0fecd34f8bf8ee99e5c1410f906e74
rich-alien <Richalien@yandex.ru>
2020-07-16 13:40:38 +0400

    add callback openTitle

--- src/components/Main/SubChooseCategoty/SubCategoryItem/SubCategoryItem.jsx
+++ src/components/Main/SubChooseCategoty/SubCategoryItem/SubCategoryItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from './SubCategoryItem.module.css'
+import {openTitle} from "../../../../Redux/state";
 
 const SubCategoryItem = (props) => {
+    let openTitleNow=()=>{
+        props.dispatch(openTitle())
+    }
     return(
-        <div className={style.container}>
+        <div onClick={openTitleNow} className={style.container}>
             {
                 props.SubCategoryName
             }
