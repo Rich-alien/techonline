a1af4d7de57c2f09dfbbbf4c2969a2649c845f66
rich-alien <Richalien@yandex.ru>
2020-07-16 20:30:57 +0400

    add close button and open button

--- src/components/Main/SubChooseCategoty/SubChooseCategory.jsx
+++ src/components/Main/SubChooseCategoty/SubChooseCategory.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import style from './SubChooseCategory.module.css'
 import SubCategoryItem from "./SubCategoryItem/SubCategoryItem";
+import {closeSubCategory} from "../../../Redux/state";
+import closeBtnSvg from "../../../close.svg";
 
 
 const SubChooseCategory = (props) => {
-    let item = props.SubCategory.map(e=><SubCategoryItem SubCategoryName={e.SubCategoryName} />)
+    let item = props.SubCategory.map(e => <SubCategoryItem
+        key={e.id}
+        SubCategoryName={e.SubCategoryName}
+        dispatch={props.dispatch}
+    />)
+    let styleDisplay = props.subCategoryDisplay;
+    let goBack =()=>{
+        props.dispatch(closeSubCategory())
+    }
     return (
-        <div className={style.container}>
-            {
-                item
-            }
+        <div style={{display: styleDisplay}} className={style.container}>
+            <div className={style.underBlock}>
+
+                <div className={style.text}>
+                    {
+                        props.titleUnderBlock.titleChooseSubCategory
+                    }
+                </div>
+                <img onClick={goBack} className={style.btn_svg} src={closeBtnSvg} alt="btn-back"/>
+            </div>
+            <div className={style.categoryContainer}>
+
+                {
+                    item
+                }
+            </div>
         </div>
+
     )
 }
 
