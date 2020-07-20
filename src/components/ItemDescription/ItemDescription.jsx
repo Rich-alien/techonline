7b2bd75edeec379d558ee9492485c5888a6232c0
rich-alien <Richalien@yandex.ru>
2020-07-20 18:55:48 +0400

    add main description , column and container for him

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -21,6 +21,22 @@ const ItemDescription = (props) => {
                     <div className={style.informationLine}>
 
                     </div>
+                    <div className={style.mainDescription}>
+                        Тонкая. Легкая. Белая.
+                        Футболка линейки B.O.M.J - первая единица контрбрендовой линии одежды. Потрясающие качество
+                        в совместительстве с уточненным подходом к деталям, которым не могут похвастаться
+                        именитые раздутые фирмы.
+                        Ткань сорта пенье
+                        Состав: 95% cotton, 5% spandex. Плотность 170 г/м²
+                    </div>
+                    <span className={style.textFotCount}>Колличество:</span>
+                    <div className={style.countContainer}>
+                        <div className={style.countMinus}>
+                            <span>-</span>
+                        </div>
+                        <div className={style.countInt}>2</div>
+                        <div className={style.countPlus}>+</div>
+                    </div>
                 </div>
             </div>
 
