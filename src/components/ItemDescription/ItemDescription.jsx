888ae9d53b60a7cde2079355ba4a8ce620d5319c
rich-alien <Richalien@yandex.ru>
2020-07-20 16:20:14 +0400

    add style for price

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import style from "./ItemDescription.module.css"
-const ItemDescription=()=>{
-    return(
+
+const ItemDescription = (props) => {
+    return (
         <div className={style.container}>
+            <h1 className={style.name}>
+                Имя товара
+                {/*{*/}
+                {/*    props.store.getState().mainPage.ObjItem.ObjName*/}
+                {/*}*/}
+            </h1>
+            <div className={style.containerDescription}>
+                <div className={style.img}>
+
+                </div>
+                <div className={style.Description}>
+                    <div className={style.price}>
+                        <span className={style.textPrice}>1477p</span>
+                    </div>
+                    <div className={style.informationLine}>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
