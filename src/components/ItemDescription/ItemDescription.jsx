04b1100a0ec328bf3bd8bf82c1d90abf02f9d9bb
rich-alien <Richalien@yandex.ru>
2020-07-20 11:14:16 +0400

    add ItemDescription and for him style

---
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./ItemDescription.module.css"
+const ItemDescription=()=>{
+    return(
+        <div className={style.container}>
+
+        </div>
+    )
+}
+export default ItemDescription;
