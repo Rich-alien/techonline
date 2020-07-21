5d188c36a3bfe779e7b76f650ca72f480b68b0f6
rich-alien <Richalien@yandex.ru>
2020-07-21 17:27:01 +0400

    add elements

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -29,13 +29,43 @@ const ItemDescription = (props) => {
                         Ткань сорта пенье
                         Состав: 95% cotton, 5% spandex. Плотность 170 г/м²
                     </div>
-                    <span className={style.textFotCount}>Колличество:</span>
-                    <div className={style.countContainer}>
-                        <div className={style.countMinus}>
-                            <span>-</span>
+                    <div className={style.countryContainer}>
+                        <div className={style.NameCountry}>
+                            Производитель:
                         </div>
-                        <div className={style.countInt}>2</div>
-                        <div className={style.countPlus}>+</div>
+                        <div className={style.country}>
+                            Япония
+                        </div>
+                    </div>
+                    <div className={style.yearContainer}>
+                        <div className={style.NameYear}>
+                            Год:
+                        </div>
+                        <div className={style.year}>
+                            2018
+                        </div>
+                    </div>
+                    <span className={style.textFotCount}>Колличество:</span>
+                    <div className={style.containerAddCard}>
+                        <div className={style.countContainer}>
+                            <div className={style.countMinus}>
+                                <span>-</span>
+                            </div>
+                            <div className={style.countInt}>2</div>
+                            <div className={style.countPlus}>+</div>
+                        </div>
+                        <div className={style.addToCard}>
+                            <span className={style.addToCard_text}>Добавить в корзину</span>
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 className="icon icon-tabler icon-tabler-arrow-narrow-right" width="44" height="44"
+                                 viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round"
+                                 stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z"/>
+                                <line x1="5" y1="12" x2="19" y2="12"/>
+                                <line x1="15" y1="16" x2="19" y2="12"/>
+                                <line x1="15" y1="8" x2="19" y2="12"/>
+                            </svg>
+                        </div>
                     </div>
                 </div>
             </div>
