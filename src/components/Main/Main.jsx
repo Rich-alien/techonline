c73347c47ff8f4426231cfb57edb27e81dc2f7e6
Richalien <Richalien@yandex.ru>
2020-07-22 18:28:03 +0400

    rename component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,8 @@ import NavNar from "./NavBar/NavBar";
 // import SubChooseCategoryContainer from "./SubChooseCategoty/SubChooseCategoryContainer";
 // import TotalItemContainer from "./TotalItem/TotalItemContainer";
 // import ChooseCarContainer from "./ChooseCar/ChooseCarContainer";
-import ItemDescription from "../ItemDescription/ItemDescription";
+// import ItemDescription from "../ItemDescription/ItemDescription";
+import ItemDescriptionContainer from "../ItemDescription/ItemDescriptionContainer";
 
 
 const Main = (props) => {
@@ -13,7 +14,7 @@ const Main = (props) => {
     return (
         <div className={style.main}>
             <NavNar/>
-            <ItemDescription store={props.store}/>
+            <ItemDescriptionContainer store={props.store}/>
             {/*<ChooseCarContainer store={props.store}/>*/}
             {/*<ChooseCategoryContainer store={props.store}/>*/}
             {/*<SubChooseCategoryContainer store={props.store}/>*/}
