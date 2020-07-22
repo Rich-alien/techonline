42122d1d5508b5830ceb3d2c62343ae4459232bb
Richalien <Richalien@yandex.ru>
2020-07-22 19:29:02 +0400

    add function for move data to Cart obj

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -3,11 +3,15 @@ import style from "./ItemDescription.module.css"
 import img from "../../img/iLoveIt.png"
 
 const ItemDescription = (props) => {
-    let onUpCount = ()=>{
+    let onUpCount = () => {
         props.upCountNow();
     }
-    let onDownCount = ()=>{
+    let onDownCount = () => {
         props.downCountNow();
+    }
+    let addOnCart = () => {
+        props.addInCartNow();
+
     }
     return (
         <div className={style.container}>
@@ -36,10 +40,10 @@ const ItemDescription = (props) => {
                     </div>
                     <div className={style.priceBlock_AddCount}>
                         <div className={style.countContainer}>
-                            <div onClick={onDownCount}  className={style.countMinus}>
+                            <div onClick={onDownCount} className={style.countMinus}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-minus"
-                                     width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50"
-                                     fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                     width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50"
+                                     fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z"/>
                                     <line x1="5" y1="12" x2="19" y2="12"/>
                                 </svg>
@@ -47,8 +51,8 @@ const ItemDescription = (props) => {
                             <div className={style.countInt}>{props.ObjCountView}</div>
                             <div onClick={onUpCount} className={style.countPlus}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus"
-                                     width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50"
-                                     fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                     width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50"
+                                     fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z"/>
                                     <line x1="12" y1="5" x2="12" y2="19"/>
                                     <line x1="5" y1="12" x2="19" y2="12"/>
@@ -61,7 +65,7 @@ const ItemDescription = (props) => {
                         </div>
                     </div>
                     <div className={style.addToCardBlock}>
-                        <button className={style.btn}>
+                        <button onClick={addOnCart} className={style.btn}>
                             Добавить в корзину
                         </button>
                     </div>
