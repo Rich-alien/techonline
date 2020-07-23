4cb750818350464c4fe9ad2ea20f77ebab861b33
Richalien <Richalien@yandex.ru>
2020-07-23 19:12:39 +0400

    default container

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "./CartItem.module.css"
+// import NavNar from "../Main/NavBar/NavBar";
+
+const CartItem = (props) =>{
+
+    return(
+        <div  className={style.container}>
+
+            <div>
+                {
+                    props.ObjName
+                }
+            </div>
+            <div>
+                {
+                    props.ObjImg
+                }
+            </div>
+            <div>
+                {
+                    props.ObjTotalPrice
+                }
+            </div>
+        </div>
+
+
+    )
+}
+export default CartItem
