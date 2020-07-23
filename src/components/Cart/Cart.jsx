f9635b7e680edf7743a76715845a1b05f9eac10e
Richalien <Richalien@yandex.ru>
2020-07-23 18:43:20 +0400

    show element mas Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import style from "./Cart.module.css"
+// import Item from "../Main/TotalItem/Item/Item";
+import CartItem from "./CartItem/CartItem";
 // import NavNar from "../Main/NavBar/NavBar";
 
-const Cart = (props) =>{
+const Cart = (props) => {
     let styleDisplay = props.styleDisplay;
-    let closeShoppingCartNow=()=>{
+    let ElementItem = props.Cart.map(e =>  <CartItem
+        key={e.id}
+        ObjName={e.ObjName}
+        ObjImg={e.ObjImg}
+        ObjTotalPrice={e.ObjTotalPrice}
+    />);
+    let closeShoppingCartNow = () => {
         props.closeShoppingCartNow();
     }
-    return(
-        <div style={{display:styleDisplay}} className={style.container}>
+
+    return (
+        <div style={{display: styleDisplay}} className={style.container}>
             <button onClick={closeShoppingCartNow} className={style.buttonBack}>Вернуться назад</button>
+            <div className={style.CartItemContainer}>
+                {
+                    ElementItem
+                }
+            </div>
         </div>
+
 
     )
 }
