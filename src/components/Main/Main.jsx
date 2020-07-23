827d2b25fdb724eb766f7abaeb731bdc2d8c0dc3
Richalien <Richalien@yandex.ru>
2020-07-23 17:16:54 +0400

    refactored component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import style from './Main.module.css'
-import NavNar from "./NavBar/NavBar";
+// import NavBar from "./NavBar/NavBar";
 // import ChooseCategoryContainer from "./ChooseCategory/ChooseCategotyContainer";
 // import SubChooseCategoryContainer from "./SubChooseCategoty/SubChooseCategoryContainer";
 // import TotalItemContainer from "./TotalItem/TotalItemContainer";
 // import ChooseCarContainer from "./ChooseCar/ChooseCarContainer";
 // import ItemDescription from "../ItemDescription/ItemDescription";
 import ItemDescriptionContainer from "../ItemDescription/ItemDescriptionContainer";
+import NavBarContainer from "./NavBar/NavBarContainer";
 
 
 const Main = (props) => {
     // debugger;
     return (
         <div className={style.main}>
-            <NavNar/>
+            <NavBarContainer store={props.store}/>
             <ItemDescriptionContainer store={props.store}/>
             {/*<ChooseCarContainer store={props.store}/>*/}
             {/*<ChooseCategoryContainer store={props.store}/>*/}
