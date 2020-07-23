310fbfe36408107f9ac1764eb30975720cece6ed
Richalien <Richalien@yandex.ru>
2020-07-23 16:59:22 +0400

    create container for separate BLL of UI

---
+++ src/components/Cart/CartContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Cart from "./Cart";
+import {closeShoppingCart} from "../../Redux/mainReducer";
+
+const CartContainer = (props) => {
+    let closeShoppingCartNow = () => {
+        props.store.dispatch(closeShoppingCart());
+    };
+    let state = props.store.getState().mainPage;
+    return (
+        <Cart
+            store={props.store}
+            styleDisplay={state.styleBlock.shoppingCart}
+            closeShoppingCartNow={closeShoppingCartNow}
+        />
+
+    )
+}
+export default CartContainer
