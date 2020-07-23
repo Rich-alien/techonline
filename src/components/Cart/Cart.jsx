944eaa889efaaecbfc44d97e312799ab691f36a4
Richalien <Richalien@yandex.ru>
2020-07-23 16:58:05 +0400

    props function close block and add button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from "./Cart.module.css"
-import NavNar from "../Main/NavBar/NavBar";
+// import NavNar from "../Main/NavBar/NavBar";
 
-const Cart = () =>{
+const Cart = (props) =>{
+    let styleDisplay = props.styleDisplay;
+    let closeShoppingCartNow=()=>{
+        props.closeShoppingCartNow();
+    }
     return(
-        <div className={style.container}>
-            <NavNar/>
+        <div style={{display:styleDisplay}} className={style.container}>
+            <button onClick={closeShoppingCartNow} className={style.buttonBack}>Вернуться назад</button>
         </div>
 
     )
