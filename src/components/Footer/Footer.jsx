62a8a354323eab437317fb96d65a472feed2a73a
Richalien <Richalien@yandex.ru>
2020-07-26 14:44:23 +0400

    default div

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import style from "./Footer.module.css"
-const Footer = ()=>{
-    return(
-        <div className={style.container}>
 
-        </div>
+const Footer = () => {
+    return (
+        <footer className={style.container}>
+            <div className={style.topFooter}>
+                <div className={style.aboutUs}>
+
+                </div>
+                <div className={style.information}>
+
+                </div>
+                <div className={style.instagram}>
+
+                </div>
+                <div className={style.contactUs}>
+
+                </div>
+
+            </div>
+            <div className={style.social}>
+                <div className={style.socialLink}>
+
+                </div>
+                <div className={style.payment}>
+
+                </div>
+            </div>
+            <div className={style.copyright}>
+
+            </div>
+        </footer>
     )
 }
 export default Footer;
