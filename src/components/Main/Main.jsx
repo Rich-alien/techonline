7ab6f1f2eca4fc148d8f5fcbf831716d8a6aeff8
Richalien <Richalien@yandex.ru>
2020-07-28 00:58:30 +0400

    use route + refactoring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import style from './Main.module.css'
-// import NavBar from "./NavBar/NavBar";
-// import ChooseCategoryContainer from "./ChooseCategory/ChooseCategotyContainer";
-// import SubChooseCategoryContainer from "./SubChooseCategoty/SubChooseCategoryContainer";
-// import TotalItemContainer from "./TotalItem/TotalItemContainer";
-// import ChooseCarContainer from "./ChooseCar/ChooseCarContainer";
-// import ItemDescription from "../ItemDescription/ItemDescription";
 import ItemDescriptionContainer from "../ItemDescription/ItemDescriptionContainer";
 import NavBarContainer from "./NavBar/NavBarContainer";
 import Footer from "../Footer/Footer";
+import SearchPage from "../SearchPage/SearchPage";
+import {BrowserRouter, Route} from "react-router-dom";
+import ProductView from "../ProductView/ProductView";
 
 
 const Main = (props) => {
     // debugger;
     return (
-        <div className={style.main}>
-            <NavBarContainer store={props.store}/>
-            <ItemDescriptionContainer store={props.store}/>
-            {/*<ChooseCarContainer store={props.store}/>*/}
-            {/*<ChooseCategoryContainer store={props.store}/>*/}
-            {/*<SubChooseCategoryContainer store={props.store}/>*/}
-            {/*<TotalItemContainer store={props.store}/>*/}
-            <Footer/>
-        </div>
-    )
-}
+      <BrowserRouter>
+          <div className={style.main}>
+              <NavBarContainer store={props.store}/>
+              <Route path='/product' render={()=> <ItemDescriptionContainer store={props.store}/> }/>
+              <Route path='/productView' render={()=> <ProductView store={props.store}/> }/>
+              <Route path='/search' render={()=> <SearchPage/> } />
 
+              <Footer/>
+          </div>
+      </BrowserRouter>)
+}
+/*<ChooseCarContainer store={props.store}/>*/
+/*<ChooseCategoryContainer store={props.store}/>*/
+/*<SubChooseCategoryContainer store={props.store}/>*/
+/*<TotalItemContainer store={props.store}/>*/
 export default Main;
