6967be0b3947c390f6d396c198ca53340e6f34b8
Richalien <Richalien@yandex.ru>
2020-07-28 01:00:42 +0400

    component for render choose items with item catlog

---
+++ src/components/ProductView/ProductView.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from '../Main/Main.module.css'
+import ChooseCarContainer from "../Main/ChooseCar/ChooseCarContainer";
+import ChooseCategoryContainer from "../Main/ChooseCategory/ChooseCategotyContainer";
+import SubChooseCategoryContainer from "../Main/SubChooseCategoty/SubChooseCategoryContainer";
+import TotalItemContainer from "../Main/TotalItem/TotalItemContainer";
+
+
+const ProductView = (props) => {
+    // debugger;
+    return (
+
+            <div className={style.main}>
+                <ChooseCarContainer store={props.store}/>
+                <ChooseCategoryContainer store={props.store}/>
+                <SubChooseCategoryContainer store={props.store}/>
+                <TotalItemContainer store={props.store}/>
+            </div>)
+}
+
+export default ProductView;
