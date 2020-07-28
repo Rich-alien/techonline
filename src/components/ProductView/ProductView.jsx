1102318f3504d6d72a283e141be135a680d44fe5
Richalien <Richalien@yandex.ru>
2020-07-28 13:45:16 +0400

    delete /*import...*/ and open comment

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import style from '../Main/Main.module.css'
 import ChooseCarContainer from "../Main/ChooseCar/ChooseCarContainer";
-// import ChooseCategoryContainer from "../Main/ChooseCategory/ChooseCategotyContainer";
-// import SubChooseCategoryContainer from "../Main/SubChooseCategoty/SubChooseCategoryContainer";
-// import TotalItemContainer from "../Main/TotalItem/TotalItemContainer";
+import ChooseCategoryContainer from "../Main/ChooseCategory/ChooseCategotyContainer";
+import SubChooseCategoryContainer from "../Main/SubChooseCategoty/SubChooseCategoryContainer";
+import TotalItemContainer from "../Main/TotalItem/TotalItemContainer";
 
 
 const ProductView = (props) => {
     return (
             <div className={style.main}>
                 <ChooseCarContainer store={props.store}/>
-                {/*<ChooseCategoryContainer store={props.store}/>*/}
-                {/*<SubChooseCategoryContainer store={props.store}/>*/}
-                {/*<TotalItemContainer store={props.store}/>*/}
+                <ChooseCategoryContainer store={props.store}/>
+                <SubChooseCategoryContainer store={props.store}/>
+                <TotalItemContainer store={props.store}/>
             </div>
     )
 }
