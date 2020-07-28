2cbaab4769c9bf2101102923f800b6f5d88fa2a1
Richalien <Richalien@yandex.ru>
2020-07-28 13:42:19 +0400

    using react-redux

--- src/components/Main/ChooseCar/ChooseCarContainer.jsx
+++ src/components/Main/ChooseCar/ChooseCarContainer.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
 import {connect} from "react-redux";
 import ChooseCar from "./ChooseCar";
-
+import {openCategory} from "../../../Redux/mainReducer";
 
 let mapStateToProps = (state) => {
-    return{
-        data:state.mainPage.data,
-        styleDisplay:state.mainPage.styleBlock.chooseCar,
-        titleUnderBlock:state.mainPage.titleUnderBlock.titleChooseCar,
+    return {
+        data: state.mainPage.data,
+        styleDisplay: state.mainPage.styleBlock.chooseCar,
+        titleUnderBlock: state.mainPage.titleUnderBlock.titleChooseCar,
+    }
+}
+let mapDispatchToProps = (dispatch) => {
+    return {
+        openCategoryNow: () => {
+            dispatch(openCategory());
+        },
     }
 }
 
-const ChooseCarContainer =  connect(mapStateToProps)(ChooseCar);
+const ChooseCarContainer = connect(mapStateToProps, mapDispatchToProps)(ChooseCar);
 
 export default ChooseCarContainer;
