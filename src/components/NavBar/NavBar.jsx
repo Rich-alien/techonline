3c634726f9e00c2d0c4be8bd3ab5503944d42691
Richalien <Richalien@yandex.ru>
2020-07-31 23:45:51 +0400

    realization class component instead functional

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,43 +3,47 @@ import style from "./NavBar.module.css"
 import CartContainer from "../Cart/CartContainer";
 import {NavLink} from "react-router-dom";
 
-const NavBar = (props) => {
-    let openShoppingCartNow = () => {
-        props.openShoppingCartNow();
+class NavBar extends React.Component {
+    openShoppingCartNow = () => {
+        this.props.openShoppingCartNow();
     }
-    return (
-        <div className={style.container}>
-            <nav className={style.nav}>
-                <NavLink className={style.linkLogo} to='/productView'>
-                    <div className={style.logo}>
-                        SLADA
-                    </div>
-                </NavLink>
-                <form className={style.searchWidth}>
-                    <input className={style.search} type="search" name="q" placeholder="Поиск по сайту"/>
-                    <NavLink to='/search'>
-                        <button className={style.btnSearch}>
-                            <img src={require("../../assets/svg-icon/search.svg")} alt=""/>
-                        </button>
+
+    render() {
+        return (
+            <div className={style.container}>
+                <nav className={style.nav}>
+                    <NavLink className={style.linkLogo} to='/productView'>
+                        <div className={style.logo}>
+                            SLADA
+                        </div>
                     </NavLink>
-                </form>
-                <NavLink className={style.linkLogo} to='/product'>
-                    <div className={style.logo}>
-                        ITEM
-                    </div>
-                </NavLink>
-                {/*<CSSTransitionGroup></CSSTransitionGroup>*/}
-                <button onClick={openShoppingCartNow} className={`${style.btnCart} ${style.btn}`}>
-                    <img src={require("../../assets/svg-icon/shopping-cart.svg")} alt=""/>
-                    <p className={style.price}>{
-                        props.CartMoney
-                    }</p>
-                    <p className={style.currency}>P</p>
-                </button>
-                <CartContainer store={props.store}/>
-                {/*    заменить картинки для btnFavorite and btnCart*/}
-            </nav>
-        </div>
-    )
+                    <form className={style.searchWidth}>
+                        <input className={style.search} type="search" name="q" placeholder="Поиск по сайту"/>
+                        <NavLink to='/search'>
+                            <button className={style.btnSearch}>
+                                <img src={require("../../assets/svg-icon/search.svg")} alt=""/>
+                            </button>
+                        </NavLink>
+                    </form>
+                    <NavLink className={style.linkLogo} to='/product'>
+                        <div className={style.logo}>
+                            ITEM
+                        </div>
+                    </NavLink>
+                    {/*<CSSTransitionGroup></CSSTransitionGroup>*/}
+                    <button onClick={this.openShoppingCartNow} className={`${style.btnCart} ${style.btn}`}>
+                        <img src={require("../../assets/svg-icon/shopping-cart.svg")} alt=""/>
+                        <p className={style.price}>{
+                            this.props.CartMoney
+                        }</p>
+                        <p className={style.currency}>P</p>
+                    </button>
+                    <CartContainer store={this.props.store}/>
+                    {/*    заменить картинки для btnFavorite and btnCart*/}
+                </nav>
+            </div>
+        )
+    }
 }
+
 export default NavBar;
