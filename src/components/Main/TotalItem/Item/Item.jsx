d35724e6d228f76dafda001df841971a70e13695
Richalien <Richalien@yandex.ru>
2020-08-01 19:31:49 +0400

    заглушка, пока не решиться проблема с путями

--- src/components/Main/TotalItem/Item/Item.jsx
+++ src/components/Main/TotalItem/Item/Item.jsx
@@ -5,29 +5,30 @@ import * as axios from "axios";
 
 class Item extends React.Component {
     componentDidMount() {
-        axios.get("http://localhost:3000/ObjItem").then(response => {
+        axios.get("http://192.168.1.104:3000/Part").then(response => {
             this.props.setItems(response.data)
         });
     }
-
     render() {
         return (
             <div className={style.container}>
                 {
                     this.props.products.map(u => <div key={u.id}>
                             <div className={style.ObjImg}>
-                                {
-                                    u.ObjImg
-                                }
+                                {/*<img src={u.images[0].url} alt="запчасть"/>*/}
+                                <img className={style.img} src={require("../../../../assets/images/productImg.png")} alt="запчасть"/>
+                                {/*{*/}
+                                {/*    console.log(u.images)*/}
+                                {/*}*/}
                             </div>
                             <div className={style.ObjName}>
                                 {
-                                    u.ObjName
+                                    u.name
                                 }
                             </div>
                             <div className={style.ObjPrice}>
                                 {
-                                    u.ObjPrice
+                                    u.price
                                 }
                             </div>
                         </div>
