45eaa7d5059e2e89da0355e22a49a6f988cd1197
Richalien <Richalien@yandex.ru>
2020-08-02 00:40:42 +0400

    class вместо функциональной

--- src/components/Main/ChooseCategory/CategoryItem/CategoryItem.jsx
+++ src/components/Main/ChooseCategory/CategoryItem/CategoryItem.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import style from './CategoryItem.module.css'
+import * as axios from "axios";
 
 
-const CategoryItem = (props) => {
-    let onOpenSubCategoryNow=()=>{
-        props.openSubCategory();
+class CategoryItem extends React.Component {
+    componentDidMount() {
+        axios.get("http://192.168.1.104:3000/Category").then(response => {
+            this.props.setCategory(response.data)
+        });
     }
-    return (
-        <button onClick={onOpenSubCategoryNow} className={style.container}>
 
-                {props.Category}
+    onOpenSubCategoryNow = () => {
+        this.props.openSubCategoryNow();
+    }
 
-        </button>
-    )
+    render() {
+        return (
+            <button onClick={this.onOpenSubCategoryNow} className={style.container}>
+                {
+                    this.props.products.map(u => <div key={u.id}>
+                        {
+                            u.name
+                        }
+                    </div>)
+                }
+            </button>
+        )
+    }
 }
+
 
 export default CategoryItem;
