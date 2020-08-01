0b3f808e0019fa6cfc265638b43dcac46dc51867
Richalien <Richalien@yandex.ru>
2020-08-01 11:17:37 +0400

    class component

--- src/components/Main/TotalItem/TotalItem.jsx
+++ src/components/Main/TotalItem/TotalItem.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
 import style from './TotalItem.module.css';
-import Item from "./Item/Item";
+import ItemContainer from "./Item/ItemContainer";
 // заменить sortUp and SortDown на иконки
 
 
-const TotalItem = (props) => {
-    let ElementItem = props.ObjItem.map(e => <Item
-        key={e.id}
-        ObjName={e.ObjName}
-        ObjImg={e.ObjImg}
-        ObjPrice={e.ObjPrice}/>)
-    let styleDisplay=props.styleDisplay;
-    let goBackNow = ()=>{
-        props.goBack();
-    }
-    return (
-        <div style={{display:styleDisplay}} className={style.container}>
-            <div className={style.SettingBar}>
+class TotalItem extends React.Component{
 
-                <div className={style.sort}>
-                    <button className={`${style.btn} ${style.StanDart}`}>
-                        {/*изменить className*/}
-                        а....я
-                    </button>
-                    <div>
-                        <div className={style.btn} onClick={goBackNow}>
-                            Вернуться назад
+
+     goBackNow = ()=>{
+        this.props.goBack();
+    }
+    render() {
+        return (
+            <div style={{display:this.props.styleDisplay}} className={style.container}>
+                <div className={style.SettingBar}>
+
+                    <div className={style.sort}>
+                        <button className={`${style.btn} ${style.StanDart}`}>
+                            {/*изменить className*/}
+                            а....я
+                        </button>
+                        <div>
+                            <div className={style.btn} onClick={this.goBackNow}>
+                                Вернуться назад
+                            </div>
+                        </div>
+                        <div>
+                            <button className={`${style.btn} ${style.sortUp}`}>
+                                по цене, дорогие
+                            </button>
+                            <button className={`${style.btn} ${style.sortDown}`}>
+                                по цене, дешевые
+                            </button>
                         </div>
                     </div>
-                    <div>
-                        <button className={`${style.btn} ${style.sortUp}`}>
-                            по цене, дорогие
-                        </button>
-                        <button className={`${style.btn} ${style.sortDown}`}>
-                            по цене, дешевые
-                        </button>
-                    </div>
                 </div>
+                {/*<div className={style.catalog}>*/}
+                {/*    {*/}
+                {/*        this.ElementItem*/}
+                {/*    }*/}
+                {/*</div>*/}
+                <ItemContainer/>
             </div>
-            <div className={style.catalog}>
-                {
-                    ElementItem
-                }
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
+
 
 export default TotalItem;
