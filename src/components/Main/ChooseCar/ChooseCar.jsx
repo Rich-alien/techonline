215e54f654f3a3bfd3eb0360042198daf386975d
Richalien <Richalien@yandex.ru>
2020-08-01 23:49:04 +0400

    использование данных из REST_API

--- src/components/Main/ChooseCar/ChooseCar.jsx
+++ src/components/Main/ChooseCar/ChooseCar.jsx
@@ -1,39 +1,33 @@
 import React from 'react';
 import style from './ChooseCar.module.css'
-import CarItem from "./CarItem/CarItem";
+import CarItemContainer from "./CarItem/CarItemContainer";
 
-const ChooseCar = (props) => {
 
-    let CarItems = props.data.map(e => <CarItem
-        onOpenCategoryNow={props.openCategoryNow}
-        key={e.id}
-        id={e.id}
-        CarName={e.CarName}
-        CarImg={e.CarImg}
-    />);
-    let styleDisplay = props.styleDisplay;
-    return (
-        <div style={{display: styleDisplay}} className={style.container}>
-            <div className={style.text}>
-                <img src={require("../../../assets/svg-icon/bulb.svg")} alt="bulb"/>
+class ChooseCar extends React.Component{
+    render() {
+        return (
+            <div style={{display: this.props.styleDisplay}} className={style.container}>
 
-                <p className={style.carText}>
-                    {
-                        props.titleUnderBlock
-                    }
-                </p>
+
+                <div className={style.carContainer}>
+                    <div className={style.text}>
+                        <img src={require("../../../assets/svg-icon/bulb.svg")} alt="bulb"/>
+
+                        <p className={style.carText}>
+                            {
+                                this.props.titleUnderBlock
+                            }
+                        </p>
+                    </div>
+                   <CarItemContainer/>
+                </div>
+
             </div>
 
-            <div className={style.carContainer}>
-                {
-                    CarItems
-                }
-            </div>
 
-        </div>
-
-
-    )
+        )
+    }
 }
+
 
 export default ChooseCar;
