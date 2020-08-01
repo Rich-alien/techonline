b5b158fa915747f1a56854f3a04cf3e5df5f7eec
Richalien <Richalien@yandex.ru>
2020-08-01 23:47:53 +0400

    использование данных из REST_API

--- src/components/Main/ChooseCar/CarItem/CarItem.jsx
+++ src/components/Main/ChooseCar/CarItem/CarItem.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import style from './CarItem.module.css'
 import carImg from '../../../../assets/images/ladaVesta2.png'
+import * as axios from "axios";
 
-const CarItem = (props) => {
-    let onOpenCategory = () => {
-        props.onOpenCategoryNow();
+class CarItem extends React.Component {
+    componentDidMount() {
+        axios.get("http://192.168.1.104:3000/Car").then(response => {
+            this.props.setCar(response.data)
+        });
     }
-    return (
-        <div onClick={onOpenCategory} className={style.container}>
-            <div className={style.CarImg}>
-                <img className={style.imgCar} src={carImg} alt="love"/>
+
+    onOpenCategory = () => {
+        this.props.openCategoryNow();
+    }
+
+    render() {
+        return (
+            <div onClick={this.onOpenCategory} className={style.container}>
+                {
+                    this.props.products.map(u => <div key={u.id}>
+                        <div className={style.CarImg}>
+                            <img className={style.imgCar} src={carImg} alt="love"/>
+                        </div>
+                        <div className={style.CarName}>
+                            <h1 className={style.CarNameH}>
+                                {
+                                   u.name
+                                }
+                            </h1>
+                        </div>
+                    </div>)
+                }
+
+
+
+
+
             </div>
-            <div className={style.CarName}>
-                <h1 className={style.CarNameH}>
-                    {
-                        props.CarName
-                    }
-                </h1>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default CarItem;
