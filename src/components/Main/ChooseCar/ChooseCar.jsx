dc09823b598710a9f140a4a8a605da560d99817f
Richalien <Richalien@yandex.ru>
2020-08-02 23:29:19 +0400

    внесение carItemContainer в родительский блок div

--- src/components/Main/ChooseCar/ChooseCar.jsx
+++ src/components/Main/ChooseCar/ChooseCar.jsx
@@ -2,32 +2,24 @@ import React from 'react';
 import style from './ChooseCar.module.css'
 import CarItemContainer from "./CarItem/CarItemContainer";
 
-
-class ChooseCar extends React.Component{
+class ChooseCar extends React.Component {
     render() {
         return (
             <div style={{display: this.props.styleDisplay}} className={style.container}>
-
-
-                <div className={style.carContainer}>
-                    <div className={style.text}>
-                        <img src={require("../../../assets/svg-icon/bulb.svg")} alt="bulb"/>
-
-                        <p className={style.carText}>
-                            {
-                                this.props.titleUnderBlock
-                            }
-                        </p>
-                    </div>
-                   <CarItemContainer/>
+                <div className={style.underText}>
+                    <img src={require("../../../assets/svg-icon/bulb.svg")} alt="bulb"/>
+                    <p className={style.carText}>
+                        {
+                            this.props.titleUnderBlock
+                        }
+                    </p>
                 </div>
-
+                <div className={style.containerSlider}>
+                    <CarItemContainer/>
+                </div>
             </div>
-
-
         )
     }
 }
-
 
 export default ChooseCar;
