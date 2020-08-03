21fac05a7ecbaa5944003df971576a7d163adbd7
Richalien <Richalien@yandex.ru>
2020-08-03 05:56:27 +0400

    добавленно условие , чтобы не было повторного заполенения, и очистка массива(может и костыль но все же работает)

--- src/components/Main/ChooseCategory/CategoryItem/CategoryItem.jsx
+++ src/components/Main/ChooseCategory/CategoryItem/CategoryItem.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import style from './CategoryItem.module.css'
 import * as axios from "axios";
+import {NavLink} from "react-router-dom";
 
 
 class CategoryItem extends React.Component {
     componentDidMount() {
-        axios.get("http://192.168.1.104:3000/Category").then(response => {
-            this.props.setCategory(response.data)
-        });
+        while (this.props.clearObj.length > 0) {
+            this.props.clearObj.pop();
+        }
+        if (this.props.products.length === 0) {
+            axios.get("http://192.168.1.104:3000/Category").then(response => {
+                this.props.setCategory(response.data)
+            });
+        }
     }
-
-
 
     render() {
         return (
-            <div className={style.container}>
+            <NavLink to='/subCategory' className={style.container}>
                 {
                     this.props.products.map(u =>
-                        <button onClick={()=>{this.props.openSubCategory(u.id)}} className={style.containerItem} key={u.id}>
-                        {
-                            u.name
-                        }
-                    </button>)
+                        <button onClick={() => {
+                            this.props.openSubCategory(u.id)
+                        }} className={style.containerItem} key={parseInt(u.id, 10)}>
+                            {
+                                u.name
+                            }
+                        </button>)
                 }
-            </div>
+            </NavLink>
         )
     }
 }
