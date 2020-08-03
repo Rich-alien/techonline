7be240674334e876b883deaad6a68e7efd5ae9a7
Richalien <Richalien@yandex.ru>
2020-08-03 06:01:34 +0400

    delete

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import style from '../Main/Main.module.css'
-import ChooseCarContainer from "../Main/ChooseCar/ChooseCarContainer";
-import ChooseCategoryContainer from "../Main/ChooseCategory/ChooseCategotyContainer";
-import SubChooseCategoryContainer from "../Main/SubChooseCategoty/SubChooseCategoryContainer";
-import TotalItemContainer from "../Main/TotalItem/TotalItemContainer";
-
 
 const ProductView = () => {
     return (
         <div className={style.main}>
-            <ChooseCarContainer/>
-            <ChooseCategoryContainer/>
-            <SubChooseCategoryContainer/>
-            <TotalItemContainer/>
+
         </div>
     )
 }
