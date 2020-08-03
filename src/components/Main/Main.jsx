5306670e67fc48091c7da94a8f9068c950988a49
Richalien <Richalien@yandex.ru>
2020-08-03 05:59:21 +0400

    роутеры важны как никогда(для каждой странички выбора по роуту)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,18 +4,24 @@ import ItemDescriptionContainer from "../ItemDescription/ItemDescriptionContaine
 import Footer from "../Footer/Footer";
 import SearchPage from "../SearchPage/SearchPage";
 import {BrowserRouter, Route} from "react-router-dom";
-import ProductView from "../ProductView/ProductView";
 import NavBarContainer from "../NavBar/NavBarContainer";
+import SubChooseCategoryContainer from "./SubChooseCategoty/SubChooseCategoryContainer";
+import TotalItemContainer from "./TotalItem/TotalItemContainer";
+import ChooseCarContainer from "./ChooseCar/ChooseCarContainer";
+import ChooseCategoryContainer from "./ChooseCategory/ChooseCategotyContainer";
 
 
-const Main = (props) => {
+const Main = () => {
     return (
         <BrowserRouter>
             <div className={style.main}>
-                <NavBarContainer store={props.store}/>
-                <Route path='/product' render={() => <ItemDescriptionContainer />}/>
-                <Route path='/productView' render={() => <ProductView />}/>
-                <Route path='/search' render={() => <SearchPage />}/>
+                <NavBarContainer/>
+                <Route path='/search' render={() => <SearchPage/>}/>
+                <Route path='/product' render={() => <ItemDescriptionContainer/>}/>
+                <Route path='/car' render={() => <ChooseCarContainer/>}/>
+                <Route path='/category' render={() => <ChooseCategoryContainer/>}/>
+                <Route path='/subCategory' render={() => <SubChooseCategoryContainer/>}/>
+                <Route path='/product' render={() => <TotalItemContainer/>}/>
                 <Footer/>
             </div>
         </BrowserRouter>
