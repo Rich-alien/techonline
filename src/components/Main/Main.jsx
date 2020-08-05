0350930b2fd8b7569958dfadc24488a879d6fb99
Richalien <Richalien@yandex.ru>
2020-08-05 15:45:00 +0400

    переход с функциональной в классовую компоненту и рендер SETCAR

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,22 +9,33 @@ import SubChooseCategoryContainer from "./SubChooseCategoty/SubChooseCategoryCon
 import TotalItemContainer from "./TotalItem/TotalItemContainer";
 import ChooseCarContainer from "./ChooseCar/ChooseCarContainer";
 import ChooseCategoryContainer from "./ChooseCategory/ChooseCategotyContainer";
+import * as axios from "axios";
 
 
-const Main = () => {
-    return (
-        <BrowserRouter>
-            <div className={style.main}>
-                <NavBarContainer/>
-                <Route path='/search' render={() => <SearchPage/>}/>
-                <Route path='/product' render={() => <ItemDescriptionContainer/>}/>
-                <Route path='/car' render={() => <ChooseCarContainer/>}/>
-                <Route path='/category' render={() => <ChooseCategoryContainer/>}/>
-                <Route path='/subCategory' render={() => <SubChooseCategoryContainer/>}/>
-                <Route path='/cart' render={() => <TotalItemContainer/>}/>
-                <Footer/>
-            </div>
-        </BrowserRouter>
-    )
+class Main extends React.Component{
+    componentDidMount() {
+        if (this.props.products.length === 0) {
+            axios.get("http://192.168.1.101:3000/Car").then(response => {
+                this.props.setCar(response.data)
+            });
+        }
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className={style.main}>
+                    <NavBarContainer/>
+                    <Route path='/search' render={() => <SearchPage/>}/>
+                    <Route path='/product' render={() => <ItemDescriptionContainer/>}/>
+                    <Route path='/car' render={() => <ChooseCarContainer/>}/>
+                    <Route path='/category' render={() => <ChooseCategoryContainer/>}/>
+                    <Route path='/subCategory' render={() => <SubChooseCategoryContainer/>}/>
+                    <Route path='/cart' render={() => <TotalItemContainer/>}/>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
 export default Main;
