6e86776b7565b509486fe8deca12482b39727287
Richalien <Richalien@yandex.ru>
2020-08-05 15:43:16 +0400

    теперь setCar происходит в MAIN

--- src/components/Main/ChooseCar/CarItem/CarItem.jsx
+++ src/components/Main/ChooseCar/CarItem/CarItem.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import style from './CarItem.module.css'
 import carImg from '../../../../assets/images/ladaVesta2.png'
-import * as axios from "axios";
+// import * as axios from "axios";
 import Carousel from "react-elastic-carousel";
 import {NavLink} from "react-router-dom";
 
 class CarItem extends React.Component {
-    componentDidMount() {
-        if (this.props.products.length === 0) {
-            axios.get("http://192.168.1.101:3000/Car").then(response => {
-                this.props.setCar(response.data)
-            });
-        }
-    }
-
-
     breakPoints = [
         {width: 1, itemsToShow: 1},
         {width: 850, itemsToShow: 2},
@@ -27,18 +18,20 @@ class CarItem extends React.Component {
                 <Carousel breakPoints={this.breakPoints}>
                     {
                         this.props.products.map(u =>
-                            <NavLink to='/category' className={style.containerItem} onClick={() => {
-                                this.props.openCategory(u.id)
-                            }} key={parseInt(u.id, 10)}>
-                                <img className={style.imgCar} src={carImg} alt="love"/>
-                                <div className={style.CarNameBlock}>
-                                    <h1 className={style.CarName}>
-                                        {
-                                            u.name
-                                        }
-                                    </h1>
-                                </div>
-                            </NavLink>
+                                <NavLink to='/category' className={style.containerItem} onClick={() => {
+                                    this.props.openCategory(u.id)
+                                }} key={parseInt(u.id, 10)}>
+                                    <img className={style.imgCar} src={carImg} alt="love"/>
+                                    <div className={style.CarNameBlock}>
+                                        <h1 className={style.CarName}>
+                                            {
+                                                u.name
+                                            }
+                                        </h1>
+                                    </div>
+                                </NavLink>
+                            //реализовать круглый карусельку для прокрутки машин, боковые уменьшены
+                            //добавить промокоды !
                         )}
                 </Carousel>
             </div>
