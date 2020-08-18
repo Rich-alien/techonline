7a4f43c2252044d379fdeeff1facd7fd62c29d1a
Richalien <Richalien@yandex.ru>
2020-08-19 01:26:24 +0400

    добавление button

--- src/components/ShareProducts/ShareProducts.jsx
+++ src/components/ShareProducts/ShareProducts.jsx
@@ -1,2 +1,25 @@
 import React from "react";
+import style from "./ShareProducts.module.css"
+import ButtonProduct from "../Button/ButtonProduct";
+class ShareProducts extends React.Component {
+    render() {
+        return (
+            <div className={style.container}>
+                <ButtonProduct  nameBtn="топ 10"/>
+                <ButtonProduct  nameBtn="топ 100"/>
+                <ButtonProduct  nameBtn="Акции"/>
+                <ButtonProduct  nameBtn="Актуальное"/>
+                <ButtonProduct  nameBtn="Спец предложение"/>
 
+            </div>
+        )
+    }
+
+}
+
+export default ShareProducts;
+// {/*className={style.first}*/}
+// {/*className={style.second}*/}
+// {/*className={style.third}*/}
+// {/*className={style.fourth}*/}
+// {/*className={style.fifth}*/}
