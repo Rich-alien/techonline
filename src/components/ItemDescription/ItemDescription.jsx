1ba1a36d18caf0dcd658d2510ef3cd4ef975655c
Richalien <Richalien@yandex.ru>
2020-08-19 23:57:48 +0400

    обертка в контейнер, для добавления отзывов

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -34,55 +34,62 @@ class ItemDescription extends React.Component {
         // debugger;
         return (
             <div className={style.container}>
-
-                {
-                    this.props.product.map(u =>
-                        <div className={style.container}>
-                            <div className={style.productImgContainer}>
-                                <div className={style.productImg}>
-                                    <img src={require("../../assets/images/productImg.png")} alt="Item"/>
-                                </div>
-                            </div>
-                            <div className={style.containerProduct}>
-                                <h1 className={style.productName}>
-                                    {u.name}
-                                </h1>
-                                <div className={style.productDirection}>
-                                    <p className={style.productDirectionText}>
-                                        {u.description}
-                                    </p>
-                                </div>
-                                <div className={style.priceBlock}>
-                                    <div className={style.priceBlock_Price}>
-                                        <p className={style.priceBlock_PriceText}>{u.price}₽</p>
-                                        <p className={style.priceBlock_PriceSubText}>за 1 штуку</p>
-                                    </div>
-                                    <div className={style.priceBlock_AddCount}>
-                                        <div className={style.countContainer}>
-                                            <div onClick={this.onDownCount} className={style.countMinus}>
-                                                <img src={require("../../assets/svg-icon/minus.svg")} alt=""/>
-                                            </div>
-                                            <div className={style.countInt}>{u.count}</div>
-                                            <div onClick={this.onUpCount} className={style.countPlus}>
-                                                <img src={require("../../assets/svg-icon/plus.svg")} alt=""/>
-                                            </div>
-                                        </div>
-                                        <div className={style.priceBlock_CountTotal}>
-                                            <p className={style.priceBlock_PriceSubTextAll}>На сумму</p>
-                                            <p className={style.priceBlock_PriceTextAll}>{u.manufacturer}₽</p>
+                <div className={style.containerItem}>
+                    {
+                        this.props.product.map((u,index) =>
+                                <div key={index} className={style.containerItemDescription}>
+                                    <div className={style.productImgContainer}>
+                                        <div className={style.productImg}>
+                                            <img src={require("../../assets/images/productImg.png")} alt="Item"/>
                                         </div>
                                     </div>
-                                    <div className={style.addToCardBlock}>
-                                        <button onClick={this.addOnCart} className={style.btn}>
-                                            Добавить в корзину
-                                        </button>
+                                    <div className={style.containerProduct}>
+                                        <h1 className={style.productName}>
+                                            {u.name}
+                                        </h1>
+                                        <div className={style.productDirection}>
+                                            <p className={style.productDirectionText}>
+                                                {u.description}
+                                            </p>
+                                        </div>
+                                        <div className={style.priceBlock}>
+                                            <div className={style.priceBlock_Price}>
+                                                <p className={style.priceBlock_PriceText}>{u.price}₽</p>
+                                                <p className={style.priceBlock_PriceSubText}>за 1 штуку</p>
+                                            </div>
+                                            <div className={style.priceBlock_AddCount}>
+                                                <div className={style.countContainer}>
+                                                    <div onClick={this.onDownCount} className={style.countMinus}>
+                                                        <img src={require("../../assets/svg-icon/minus.svg")} alt=""/>
+                                                    </div>
+                                                    <div className={style.countInt}>{u.count}</div>
+                                                    <div onClick={this.onUpCount} className={style.countPlus}>
+                                                        <img src={require("../../assets/svg-icon/plus.svg")} alt=""/>
+                                                    </div>
+                                                </div>
+                                                <div className={style.priceBlock_CountTotal}>
+                                                    <p className={style.priceBlock_PriceSubTextAll}>На сумму</p>
+                                                    <p className={style.priceBlock_PriceTextAll}>{u.manufacturer}₽</p>
+                                                </div>
+                                            </div>
+                                            <div className={style.addToCardBlock}>
+                                                <button onClick={this.addOnCart} className={style.btn}>
+                                                    Добавить в корзину
+                                                </button>
+                                            </div>
+
+                                        </div>
+
                                     </div>
 
                                 </div>
-
-                            </div>
-                        </div>)
-                }
+                        )
+                    }
+                </div>
+                <div className={style.containerRewiw}>
+                  <p className={style.headerText}>Отзывы</p>
+                    <img src={require("../../assets/svg-icon/arrowDown.svg")} alt="Down"/>
+                </div>
             </div>
 
         )
